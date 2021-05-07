5c562cd5a51a82218d6f3a16168123f34c3361dd
rnzid <ranjitdangol13@gmail.com>
2021-05-08 00:36:53 +0545

    Logout database migration and validation crud complete

--- AppointmentScheduling/Startup.cs
+++ AppointmentScheduling/Startup.cs
@@ -28,7 +28,7 @@ namespace AppointmentScheduling
         {
             services.AddDbContext<ApplicationDbContext>(option => option.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllersWithViews();
-            services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>();
+            services.AddIdentity<ApplicationUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -48,6 +48,8 @@ namespace AppointmentScheduling
             app.UseStaticFiles();
 
             app.UseRouting();
+
+            app.UseAuthentication();
 
             app.UseAuthorization();
 
