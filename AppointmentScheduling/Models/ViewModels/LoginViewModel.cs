4ab47a6676487301f495f2753f1047712bd37c00
rnzid <ranjitdangol13@gmail.com>
2021-05-07 14:40:22 +0545

    Add Login View Model

---
+++ AppointmentScheduling/Models/ViewModels/LoginViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppointmentScheduling.Models.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public String Password { get; set; }
+        [Display(Name ="Remember Me?")]
+        public bool RememberMe { get; set; }
+    }
+}
